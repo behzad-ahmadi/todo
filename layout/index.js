d11134530cc187b2bd8a6df78af0a76930e01c3b
behzad <bh.ahmady@gmail.com>
2023-05-05 01:52:45 +0430

    .

--- layout/index.js
+++ layout/index.js
@@ -1,3 +1,12 @@
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
 export default function Layout() {
   return <></>;
 }
