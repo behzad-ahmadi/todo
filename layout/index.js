6bdbbd2b40f13558b47fba87b216bbea7fbf0e0b
behzad <bh.ahmady@gmail.com>
2023-05-06 17:39:02 +0430

    .

--- layout/index.js
+++ layout/index.js
@@ -1,8 +1,4 @@
-import { Button } from '@mui/base';
-import { useAppTheme } from '@/context/themeContext';
-import { Box, IconButton, Paper, useTheme } from '@mui/material';
-import Brightness4Icon from '@mui/icons-material/Brightness4';
-import Brightness7Icon from '@mui/icons-material/Brightness7';
+import { Box } from '@mui/material';
 
 import AppBarSection from './appBar';
 
