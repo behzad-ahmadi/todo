0cfb2aaffb760bcfcb9f100d3f06f50e14b55849
behzad <bh.ahmady@gmail.com>
2023-05-08 21:43:31 +0430

    add mongo db

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,28 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { dbPass, username } from '@/lib/db';
+import { MongoClient } from 'mongodb';
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+const uri = `mongodb+srv://behzad:yexATLvuzfPyYDIY@cluster0.7ebsvtl.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri);
+
+const run = async () => {
+  try {
+    const db = client.db('todo_db');
+    const todo = db.collection('todo');
+
+    await todo.insertOne({ uid: 1, items: { title: 'first' } });
+
+    return true;
+  } catch (error) {
+    console.log('db Error', error);
+  } finally {
+    client.close;
+  }
+};
+
+export default async function handler(req, res) {
+  const result = await run();
+
+  if (result) res.status(201).json({ result: 'Inserted' });
+  else res.status(400).json({ result: 'ERROR' });
 }
