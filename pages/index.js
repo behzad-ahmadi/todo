c519413b118d81edff5432c3d8d3c8adb723bc07
behzad <bh.ahmady@gmail.com>
2023-05-22 02:47:56 +0330

    todo list

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,24 @@
-export default function Home() {
-  return <></>;
+import Todos from '@/components/pages/todo';
+import { getAllTodos } from '@/lib/api-util';
+
+export default function Home({ todos }) {
+  console.log('res', todos);
+
+  return <Todos todos={todos} />;
+}
+
+export async function getStaticProps() {
+  let todos = {};
+  try {
+    todos = await getAllTodos();
+  } catch (error) {
+    console.log('getStaticProps', error);
+  }
+
+  return {
+    props: {
+      todos,
+    },
+    revalidate: 5,
+  };
 }
